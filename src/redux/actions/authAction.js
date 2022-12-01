a0da051a8d83771b8dbe3240db54905753daaa4a
Roberto Athos <robertoathos22@gmail.com>
2022-12-01 08:42:13 -0300

    new api url

--- src/redux/actions/authAction.js
+++ src/redux/actions/authAction.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import {toast} from 'react-toastify'
 
-const url = 'https://irpf-api.herokuapp.com/user'
+const url = 'https://imposto-de-renda-api.onrender.com/user'
 
 
 export const userRegister =  user => {
